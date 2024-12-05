b01b9257ddec777a09c3f6a081e6f5a68bb74de1
kapiw04 <wojtowicz.kacper@gmail.com>
2024-12-05 07:03:32 +0100

    days 1-5

---
+++ day3/main.py
@@ -0,0 +1,34 @@
+import re
+
+with open("input") as f:
+    text = f.read()
+
+# regex = re.findall(r"mul\((\d+),(\d+)\)", text)
+
+# text = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+regex = re.split(r"(do\(\)|don't\(\))", text)
+# print(sum(int(x) * int(y) for x, y in regex))
+
+on=True
+
+s = 0
+
+for i in regex:
+    if i == "don't()":
+        on = False
+    elif i == "do()":
+        on = True
+
+    if on:
+        regex = re.findall(r"mul\((\d+),(\d+)\)", i)
+        s += sum(int(x) * int(y) for x, y in regex)
+
+print(s)
+
+# s1 = 0
+# regex = re.findall(r"do\(\)((?!.*do\(\)))don't\(\)", text)
+# for t in regex:
+#     print(t)
+#     s1 += sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d+),(\d+)\)", t))
+
+# print(s1)
